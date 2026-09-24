19e40e3e4aff2d221ace774af942c61c1486ab49
gz <gzking52@gmail.com>
2026-09-24 18:10:29 -0400

    Atualizar o form.php

--- paginas/calendario/form.php
+++ paginas/calendario/form.php
@@ -25,8 +25,7 @@ $tipo = $evento['tipo'] ?? '';
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="../../css/calendario.css">
-
+    <link rel="stylesheet" href="/css/calendario.css">
 </head>
 
 <body>

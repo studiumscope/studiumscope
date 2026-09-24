ac053cfaab6b7e30b9c0fdd0d087081ea9d2208e
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-09-24 12:14:59 -0400

    Add Bootstrap and custom CSS links to header

--- _cabecalho.php
+++ _cabecalho.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="<?= '/s' ?> rel="stylesheet">
     <title>Studium</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/stylesheet.css" rel="stylesheet">
+    <link href="/css/disciplinas.css" rel="stylesheet">
 </head>
 
 <body>

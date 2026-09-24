bc4ffe563d6b892bf32f9414d4a4c454875d5871
gz <gzking52@gmail.com>
2026-09-24 18:09:53 -0400

    Atualizar o lista.php

--- paginas/calendario/lista.php
+++ paginas/calendario/lista.php
@@ -57,7 +57,7 @@ $hoje = date('Y-m-d');
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="../../css/calendario.css">
+    <link rel="stylesheet" href="/css/calendario.css">
 </head>
 
 <body>

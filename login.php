8d802c34fdbb277868a539136b5a5ec43c7024af
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-09-24 12:17:42 -0400

    Refactor login form layout and styling

--- login.php
+++ login.php
@@ -1,17 +1,19 @@
-<div class="card mt-5">
+<div class="card mt-5 sem-bordas">
     <div class="card-body">
         <form method="post" action="authController.php?acao=login">
-
-            <label class="form-label">E-mail:</label>
-            <input class="form-control" type="email" name="email"
-                   value="<?= $dado["email"] ?? '' ?>" required autofocus>
-
-            <label class="form-label">Senha:</label>
-            <input class="form-control" type="password" name="senha"
-                   value="<?= $dado["senha"] ?? '' ?>" required autofocus>
+            <div class="insere-azul d-flex align-items-center">
+                <label class="form-label">E-mail:</label>
+                <input class="form-control" type="email" name="email"
+                value="<?= $dado["email"] ?? '' ?>" required autofocus>
+            </div>
+            <div class="insere-azul d-flex align-items-center">
+                <label class="form-label">Senha:</label>
+                <input class="form-control" type="password" name="senha"
+                value="<?= $dado["senha"] ?? '' ?>" required autofocus>
+            </div>
 
             <a class="btn btn-link mt-4" href="authController.php?acao=cadastrar" >Cadastrar</a>
-            <button class="btn btn-primary mt-4" type="submit">Entrar</button>
+            <button class="btn mt-4 bg-transparent sem-bordas text-black" type="submit">Entrar</button>
         </form>
     </div>
 </div>

4b4cbe50dfc28db6a457c0e17acc09f052a18042
LUIZCERVIERI <264819212+LUIZCERVIERI@users.noreply.github.com>
2026-09-24 22:52:12 -0400

    css acaba

--- _cabecalho.php
+++ _cabecalho.php
@@ -4,28 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="<?= '/s' ?> rel="stylesheet">
     <title>Studium</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/stylesheet.css" rel="stylesheet">
     <link href="/css/disciplinas.css" rel="stylesheet">
-    <link href="/php/studiumscope/css/trabalhos.css" rel="stylesheet">
+    <link href="/css/trabalhos.css" rel="stylesheet">
 </head>
 
 <body>
-    
-
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="../../../home.php">Studium</a>
+            <a class="navbar-brand" href="/home.php">Studium</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../../../index.php">Início</a>
+                        <a class="nav-link active" aria-current="page" href="/index.php">Início</a>
                     </li>
                 </ul>
             </div>

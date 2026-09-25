aefd547cb84455c7344246bead180720f8811bfb
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-09-24 20:14:57 -0400

    Update _cabecalho.php

--- _cabecalho.php
+++ _cabecalho.php
@@ -17,7 +17,7 @@
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="../../home.php">Studium</a>
+            <a class="navbar-brand" href="../../../home.php">Studium</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -25,7 +25,7 @@
                 
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../../index.php">Início</a>
+                        <a class="nav-link active" aria-current="page" href="../../../index.php">Início</a>
                     </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">.</a>

6e3730ddc4fea0fb785ee28d0f03ada137a81e93
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-09-24 20:08:44 -0400

    Remove unnecessary closing div tag in footer

--- _rodape.php
+++ _rodape.php
@@ -1,7 +1,6 @@
 <hr class="mt-5">
         <p class="mb-0">Sistema Studium &copy; <?= date("d/m/Y") ?> | Desenvolvido para todos(as) os(as) discentes!</p>
         <p class="mb-0 text-muted">@1145 - versão scope 1.0</p>
-    </div>
 
 
     <script src="./js/bootstrap.bundle.min.js"></script>
